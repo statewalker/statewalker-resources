24e9ace604052d1f7c4a9d87eaa0e203d79e88ef
Mael Van den Plas <mael.vdp@gmail.com>
2023-03-01 09:09:14 +0100

    add Notebook adapter

--- src/workspace/Project.js
+++ src/workspace/Project.js
@@ -1,13 +1,14 @@
 import ResourceAdapter from "../core/ResourceAdapter.js";
 import Workspace from "./Workspace.js";
-import JsonAdapter from "../core/JsonAdapter.js";
+import { resolveUrl,concatPath } from "@statewalker/uris";
 
 export default class Project extends ResourceAdapter {
-
+/*
   async getManifest() {
     const jsonAdapter = this.requireAdapter(JsonAdapter);
     return await jsonAdapter.getJson();
   }
+*/
 
   get projectName() {
     const pathSegments = this.path.split("/").filter((s) => !!s);
@@ -16,5 +17,14 @@ export default class Project extends ResourceAdapter {
 
   get workspace() {
     return this.repository.getAdapter(Workspace);
+  }
+
+  resolveProjectPath(path){
+    return concatPath(resolveUrl(this.path, "."), path)
+  }
+
+  async getProjectResource(path, create = false) {
+    const resourcePath=this.resolveProjectPath(path)
+    return this.repository.getResource(resourcePath, create)
   }
 }
--- src/workspace/index.js
+++ src/workspace/index.js
@@ -1,2 +1,4 @@
 export { default as Workspace } from "./Workspace.js";
 export { default as Project } from "./Project.js";
+export { default as Notebook } from "./Notebook.js";
+
