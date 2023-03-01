bb80b7ce0303945bee52079e42da00a1c67ecf97
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-03-01 05:48:54 +0100

    add missing exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import * as core from "./core/index.js";
 import * as utils from "./utils/index.js";
+import * as workspace from "./workspace/index.js";
 export {
   core,
-  utils
+  utils,
+  workspace
 }
